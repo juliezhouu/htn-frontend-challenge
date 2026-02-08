9cfb4b7485b41ff579438827b7b2673d8342a703
juliezhouu <juliezhouxt@gmail.com>
2026-02-08 18:27:39 -0500

    added visuals

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/htn-challenge/',
+  base: '/htn-frontend-challenge/',
 })
